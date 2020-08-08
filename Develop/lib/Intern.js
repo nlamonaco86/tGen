6737cde9f70e5f864210eb81008ee11a253d0387
Nicholas La Monaco <nlamonaco86@gmail.com>
2020-08-08 17:10:56 -0400

    Tests all PASS, objects created

---
+++ Develop/lib/Intern.js
@@ -0,0 +1,18 @@
+// TODO: Write code to define and export the Intern class.  HINT: This class should inherit from Employee.
+const Employee = require("./Employee")
+
+class Intern extends Employee {
+    constructor(name, id, email, github){
+        super(name, id, email)
+        this.school = "UCLA";
+    }
+    getSchool(){
+        return this.school
+    }
+
+    getRole(){
+        return "Intern";
+    }
+}
+
+module.exports = Intern;
