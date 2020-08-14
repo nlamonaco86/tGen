60b9fa85a7ebf62e5b3e8c0555bd7de18ee6cf4a
Nicholas La Monaco <nlamonaco86@gmail.com>
2020-08-14 11:39:09 -0400

    app completed!

--- Develop/app.js
+++ Develop/app.js
@@ -15,100 +15,82 @@ const team = [];
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 
-//ask the user about the team
-//additional questions prompt if member is manager, intern or engineer
 //information is put into a new object
 //those objects are stored in the array
-function createTeam() {
+//ask about each team member
+function createTeam(){
+inquirer.prompt([
+    {
+        type: "input",
+        message: "Please enter employee name:",
+        name: "name",
+    },
+    {
+        type: "list",
+        message: "Choose Employee Role:",
+        choices: ["Intern", "Engineer", "Manager"],
+        name: "role"
+    },
+    {
+        type: "input",
+        message: "Employee ID:",
+        name: "id",
+    },
+    {
+        type: "input",
+        message: "E-Mail Address:",
+        name: "email",
+    },
+    {
+        type: "input",
+        message: "Github Username:",
+        name: "github",
+    },
+    {
+        type: "input",
+        message: "Office Number:",
+        name: "officeNumber",
+    },
+    {
+        type: "input",
+        message: "School:",
+        name: "school",
+    },
+    {
+        type: "confirm",
+        message: "Add another Employee?",
+        name: "another",
+    },
+
+]).then(function (response) {
     var teamMember = {};
-    inquirer.prompt([
-        {
-            type: "input",
-            message: "How many people are on the team?",
-            name: "staffSize",
-        },
-    ]) 
-    .then (function (response) {
-        console.log(response.staffSize)
-        inquirer.prompt([
-            {
-                type: "input",
-                message: "Please enter employee name:",
-                name: "name",
-            },
-            {
-                type: "list",
-                message: "Choose Employee Role:",
-                choices: ["Intern", "Engineer", "Manager"],
-                name: "role"
-            },
-            {
-                type: "input",
-                message: "Employee ID:",
-                name: "id",
-            },
-            {
-                type: "input",
-                message: "E-Mail Address:",
-                name: "email",
-            },
-            {
-                type: "input",
-                message: "Github Username:",
-                name: "github",
-            },
-            {
-                type: "input",
-                message: "Office Number:",
-                name: "officeNumber",
-            },
-            {
-                type: "input",
-                message: "School:",
-                name: "school",
-            },    
-           
-        ]) .then(function (response) {
-            if (response.role === "Manager") {
-                teamMember = new Manager(response.name, response.id, response.email, response.officeNumber);
-                
-            } else if (response.role === "Engineer") {
-                teamMember = new Engineer(response.name, response.id, response.email, response.github);
-                
-            } else {
-                teamMember = new Intern(response.name, response.id, response.email, response.school);
-            }
-        
-            team.push(teamMember);
-            
-            console.log(teamMember)
-            console.log("-----------------")
-            console.log(team)
-            
-    });
- 
-    });
-   //ask how many people are on the team, then run the function that many times with an adjustable loop
+    //create objects based on employee role
+    if (response.role === "Manager") {
+        teamMember = new Manager(response.name, response.id, response.email, response.officeNumber);
 
-};
+    }
+    else if (response.role === "Engineer") {
+        teamMember = new Engineer(response.name, response.id, response.email, response.github);
 
+    }
+    else {
+        teamMember = new Intern(response.name, response.id, response.email, response.school);
+    }
+    //push the team member object to the array
+    team.push(teamMember);
+    //repeat the function if user needs another team member
+    if (response.another) {
+        createTeam();
+    }
+    //output to the team directory
+    else {
+        fs.writeFile('./output/team.html', render(team), (err) => {
+            if (err) return console.log(err);
+            console.log("success");
+        });
+    }
+});
+}
+
+// call the function
 createTeam();
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
